0ff9f61bf660413af43de37edd7a28ddae76cc39
JJ <tresvecesnetflix@gmail.com>
2026-04-15 18:35:18 -0500

    Agregar archivos de proyecto.

---
+++ webApiRepo/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace webApiRepo.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
